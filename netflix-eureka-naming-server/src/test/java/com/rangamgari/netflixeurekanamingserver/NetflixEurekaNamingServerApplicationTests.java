91a508796f04366893b5fb7ac5c1506b44507f42
ravi <ravi@kimeralabs.com>
2020-01-24 13:56:29 -0500

    Added Eureka

---
+++ netflix-eureka-naming-server/src/test/java/com/rangamgari/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangamgari.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
