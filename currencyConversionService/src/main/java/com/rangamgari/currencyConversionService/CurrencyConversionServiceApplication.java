ebfbe2d36619f3c195773e49323e2a53dbfc36f4
ravi <ravi@kimeralabs.com>
2020-01-24 10:35:01 -0500

    Feign Proxy Implemented

--- currencyConversionService/src/main/java/com/rangamgari/currencyConversionService/CurrencyConversionServiceApplication.java
+++ currencyConversionService/src/main/java/com/rangamgari/currencyConversionService/CurrencyConversionServiceApplication.java
@@ -2,7 +2,9 @@ package com.rangamgari.currencyConversionService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.rangamgari.currencyConversionService")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
--- currencyConversionService/src/main/java/com/rangamgari/currencyConversionService/controller/CurrencyConversionController.java
+++ currencyConversionService/src/main/java/com/rangamgari/currencyConversionService/controller/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.rangamgari.currencyConversionService.controller;
 
 import com.rangamgari.currencyConversionService.domain.CurrencyConversionBean;
+import com.rangamgari.currencyConversionService.proxy.CurrencyExchangeServiceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ import java.util.Map;
 @RestController
 public class CurrencyConversionController {
   @Autowired private Environment env;
+
+  @Autowired private CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
 
   @GetMapping("currency-conversion/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversionBean convertCurrency(
@@ -36,28 +39,22 @@ public class CurrencyConversionController {
         to,
         currencyConversionBean.getConversionMultiple(),
         Long.parseLong(quantity),
-            Long.parseLong(env.getProperty("local.server.port")));
+        Long.parseLong(env.getProperty("local.server.port")));
   }
-  @GetMapping("currency-conversion-fiegn/{from}/to/{to}/quantity/{quantity}")
+
+  @GetMapping("currency-conversion-feign/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversionBean convertCurrencyFeign(
-          @PathVariable String from, @PathVariable String to, @PathVariable String quantity) {
-    Map<String, String> uriVariables = new HashMap<>();
-    uriVariables.put("from", from);
-    uriVariables.put("to", to);
-    ResponseEntity<CurrencyConversionBean> responseEntity =
-            new RestTemplate()
-                    .getForEntity(
-                            "http://localhost:8001/currency-exchange/{from}/to/{to}",
-                            CurrencyConversionBean.class,
-                            uriVariables);
-    CurrencyConversionBean currencyConversionBean = responseEntity.getBody();
-    System.out.println(responseEntity.getBody().getConversionMultiple());
+      @PathVariable String from, @PathVariable String to, @PathVariable String quantity) {
+
+    CurrencyConversionBean currencyConversionBean =
+        currencyExchangeServiceProxy.retrieveExchangeValue(from, to);
+    System.out.println(currencyConversionBean.getConversionMultiple());
     return new CurrencyConversionBean(
-            currencyConversionBean.getId(),
-            from,
-            to,
-            currencyConversionBean.getConversionMultiple(),
-            Long.parseLong(quantity),
-            Long.parseLong(env.getProperty("local.server.port")));
+        currencyConversionBean.getId(),
+        from,
+        to,
+        currencyConversionBean.getConversionMultiple(),
+        Long.parseLong(quantity),
+        Long.parseLong(env.getProperty("local.server.port")));
   }
 }
